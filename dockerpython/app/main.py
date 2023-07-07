500c852e105cffbaac860331cf08d90cb0987766
mhkamal1 <124262740+mhkamal1@users.noreply.github.com>
2023-07-07 12:42:27 +0200

    Add files via upload

---
+++ dockerpython/app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8000, host='0.0.0.0')
